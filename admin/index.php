baa0b4b5bbb2df35cc3279304b757aa28d9c2c3e
Hentsuna <damtrongj095@gmail.com>
2025-05-11 04:16:33 +0700

    update admin

---
+++ admin/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include '../db.php'; ?>
+    <title>Trang Admin</title>
+    <link rel="icon" href="<?php echo $icon; ?>">
+    <link rel="stylesheet" href="include/fontawesome/css/all.css">
+    <link rel="stylesheet" href="include/style/bootstrap.css">
+    <link rel="stylesheet" href="admin.css">
+</head>
+
+<body>
+    <?php
+    session_start();
+
+    if (!isset($_SESSION['loginadmin'])) {
+        if (isset($_POST['username']) && isset($_POST['password'])) {
+            $username = mysqli_real_escape_string($conn, $_POST['username']);
+            $password = $_POST['password'];
+
+            $getdata = mysqli_query($conn, "SELECT password, role FROM users WHERE username = '$username'");
+            $user = mysqli_fetch_assoc($getdata);
+
+            if ($user && password_verify($password, $user['password'])) {
+                if ($user['role'] === 'admin') {
+                    $_SESSION['loginadmin'] = true;
+                    $_SESSION['user'] = $username;
+                    $_SESSION['user_role'] = 'admin';
+                } else {
+                    echo '<script>alert("Bạn không có quyền truy cập trang quản trị.")</script>';
+                }
+            } else {
+                echo '<script>alert("Tài khoản hoặc mật khẩu không đúng.")</script>';
+            }
+        }
+    }
+
+    if (isset($_POST['act']) && $_POST['act'] === 'logout') {
+        session_destroy();
+        header("Location: index.php");
+        exit();
+    }
+
+    if (isset($_SESSION['loginadmin']) && $_SESSION['user_role'] === 'admin') {
+        include 'trangquanly.php';
+    } else {
+        include 'login.php';
+    }
+    ?>
+</body>
+
+</html>
---
+++ admin/login.php
@@ -0,0 +1,18 @@
+<div class="box-login">
+    <form method="post" action="">
+        <div style="text-align: center">
+            <h4>Đăng nhập quản trị</h4>
+        </div>
+        <div class="form-group">
+            <label for="username">Tên tài khoản</label>
+            <input type="text" class="form-control" id="username" required name="username">
+        </div>
+        <div class="form-group">
+            <label for="password">Mật khẩu</label>
+            <input type="password" class="form-control" id="password" required name="password">
+        </div>
+        <div style="text-align: center">
+            <button type="submit" class="btn btn-primary mt-3">Đăng nhập</button>
+        </div>
+    </form>
+</div>
